4423628e193e49cf016a14d9bb44955bd2a74741
Martin Engelin <marwwinn@gmail.com>
2012-07-06 13:36:45 +0200

    Initial Commit

---
+++ Tilerpg/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Tilerpg
+//
+//  Created by T2 on 7/6/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
